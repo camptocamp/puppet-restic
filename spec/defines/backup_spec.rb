9b2ed16c22369838682aa7b472487971fb5786f9
lconsuegra <lenny.consuegra@camptocamp.com>
2020-08-18 15:24:57 +0200

    spec: Fix rspec testing

--- spec/defines/backup_spec.rb
+++ spec/defines/backup_spec.rb
@@ -3,8 +3,11 @@ require 'spec_helper'
 describe 'restic::backup' do
   let(:title) { 'namevar' }
   let(:params) do
-    {}
+    { 'files' => '/var/backups',
+      'repo'  => 's3:s3.amazonaws.com/bucket_backups',
+    }
   end
+  let(:pre_condition) { 'include restic' }
 
   on_supported_os.each do |os, os_facts|
     context "on #{os}" do
