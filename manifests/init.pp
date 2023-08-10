deccd4a92625e89327f712cb4a66d5ec32907c38
Christophe Burki <christophe.burki@camptocamp.com>
2023-08-10 15:05:43 +0200

    Revert "feat: binary cannot be updated"
    This reverts commit 41a42bb28e49ccbbf85f30bbc2f06370bd8b9db4.

--- manifests/init.pp
+++ manifests/init.pp
@@ -14,23 +14,19 @@ class restic(
     ensure          => present,
     extract         => true,
     extract_path    => '/usr/local/bin',
-    extract_command => "bunzip2 -c %s > /usr/local/bin/restic-${version}",
+    extract_command => 'bunzip2 -c %s > /usr/local/bin/restic',
     source          => "https://github.com/restic/restic/releases/download/v${version}/restic_${version}_linux_amd64.bz2",
     checksum        => $checksum,
     checksum_type   => $checksum_type,
     cleanup         => true,
-    creates         => "/usr/local/bin/restic-${version}",
+    creates         => '/usr/local/bin/restic',
     require         => Package['bzip2'],
   }
-  -> file { "/usr/local/bin/restic-${version}":
+  -> file { '/usr/local/bin/restic':
     ensure => file,
     mode   => '0755',
     owner  => 'root',
     group  => 'root',
-  }
-  -> file { '/usr/local/bin/restic':
-    ensure => link,
-    target => "/usr/local/bin/restic-${version}",
   }
   file { '/var/log/restic':
     ensure => directory,
