41a42bb28e49ccbbf85f30bbc2f06370bd8b9db4
Christophe Burki <christophe.burki@camptocamp.com>
2023-08-10 14:36:30 +0200

    feat: binary cannot be updated

--- manifests/init.pp
+++ manifests/init.pp
@@ -14,19 +14,23 @@ class restic(
     ensure          => present,
     extract         => true,
     extract_path    => '/usr/local/bin',
-    extract_command => 'bunzip2 -c %s > /usr/local/bin/restic',
+    extract_command => "bunzip2 -c %s > /usr/local/bin/restic-${version}",
     source          => "https://github.com/restic/restic/releases/download/v${version}/restic_${version}_linux_amd64.bz2",
     checksum        => $checksum,
     checksum_type   => $checksum_type,
     cleanup         => true,
-    creates         => '/usr/local/bin/restic',
+    creates         => "/usr/local/bin/restic-${version}",
     require         => Package['bzip2'],
   }
-  -> file { '/usr/local/bin/restic':
+  -> file { "/usr/local/bin/restic-${version}":
     ensure => file,
     mode   => '0755',
     owner  => 'root',
     group  => 'root',
+  }
+  -> file { '/usr/local/bin/restic':
+    ensure => link,
+    target => "/usr/local/bin/restic-${version}",
   }
   file { '/var/log/restic':
     ensure => directory,
